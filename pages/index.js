bd30e3f3df2c53840d402dd9e6938945fc0f80cf
Anderson Rodrigues <161659298+andertrx@users.noreply.github.com>
2024-05-21 14:09:47 +0000

    Adiciona os scripts `lint:check`e  `linch:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1>O único dia fácil foi ontem! @ </h1>
+function Home() {
+  return <h1>O único dia fácil foi ontem! @ </h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
+}
 export default Home;
